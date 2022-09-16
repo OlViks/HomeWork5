0835cf63db5e777d68bdfc142ed75dd3361eb04d
OlViks <salova.40@yandex.ru>
2022-09-16 23:16:39 +0300

    Начало

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    System.out.println("Задание №5");
+        //Задание 1
+        int age = 15;
+        if (age >= 18) {
+            System.out.println ("Виу, ВиУ, Поздравляем с совершеннолетием");
+        }
+        if (age < 18) {
+            System.out.println ("Возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
+        }
+    }
+}
