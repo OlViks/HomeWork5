fb1e0c1cd9b56812fe3b71d2a9a3c40bd6d70f05
OlViks <salova.40@yandex.ru>
2022-09-17 00:45:47 +0300

    задания 5.1

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,49 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание №5");
-        ///Задание 1.1
-        int age = 24;
-        if (age >= 18) {
-            System.out.println ("Виу, ВиУ, Поздравляем с совершеннолетием");
-        } else {
-            System.out.println ("Возраст совершеннолетия ещё не наступил, и нужно немного подождать.");
+        ///Задание 1.2
+        //Задание 1
+        int age = 3;
+        if (age >= 2 && age <= 6) {
+            System.out.println ("Если возраст человека  " + age + " , то ему нужно ходить в детский сад.");
+        } else if (age >= 7  && age < 18) {
+            System.out.println ("Если возраст человека  " + age + " , то ему нужно ходить в школу.");
+        } else if (age >= 18  && age < 24) {
+            System.out.println ("Если возраст человека  " + age + " , то ему нужно ходить в университет.");
+        }else {
+            System.out.println ("Если возраст человека  " + age + " , то ему нужно ходить на работу.");
         }
+        //Задание 2
 
-        //Задание 2.1
-
-        if (age >= 7 && age < 18) {
-            System.out.println ("Ребенок ходит в школу.");
-        } else if (age >=18 && age < 24) {
-            System.out.println ("Человек уже закончил школу и может отправляться в университет.");
-        }else if (age >=24) {
-            System.out.println ("Человек окончил университет и ему пора искать первую работу.");
+        if (age < 5 ) {
+            System.out.println ("Если возраст человека  " + age + " ,  он не может кататься на аттракционе. ");
+        } else if (age >= 5  && age < 14) {
+            System.out.println ("Если возраст человека  " + age + " ,  то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя. ");
+        }else {
+            System.out.println ("Если возраст человека равен " + age + " он может кататься без сопровождения взрослого.");
         }
-
-        //Задние 3.1
-
-        int capacity = 102;
-        int seatCapasity = 60;
-        int standCapasity = capacity - seatCapasity;
-        int seatUsed = 30;
-        int standUsed = 15;
-        if (seatUsed < seatCapasity){
-            System.out.println ("Есть еще " + (seatCapasity - seatUsed) + " сидчих мест.");
-        } else {
-            System.out.println("Стоячих мест нет.");
+        //Задание 3
+        int one = 1011;
+        int two = 2;
+        int three = 2;
+        if (one > two) {
+            if (one >= three) {
+                System.out.println("Большее значение - " + one);
+            }else{
+                System.out.println("Большее значение - " + three);
+            }
+        } else if (two > one) {
+            if (two >= three) {
+                System.out.println("Большее значение - " + two);
+            }else{
+                System.out.println("Большее значение - " + three);
+            }
+        }else{
+            if (three > one) {
+            }else if (three > two) {
+                System.out.println("Большее значение - " + three);
+            }
         }
-        if (standUsed < standCapasity){
-            System.out.println ("Есть еще " + (standCapasity - standUsed) + " стоячих мест.");
-        } else {
-            System.out.println("Сидячих мест нет.");
-        }
-
 
     }
 }
